1c977e86d5085e95d8f1c1439e64c5ac3fb8caf4
Emmanuel Rodriguez <erodg2013@example.com>
2020-01-02 22:50:00 -0600

    Antsle k8s cluster

--- antsle/terraform/main.tf
+++ antsle/terraform/main.tf
@@ -6,7 +6,7 @@ provider "antsle" {
 resource "antsle_antlets" "proxy" {
   dname = "k8s1-proxy"
   template = "CentOS-7"
-  ram = 1024
+  ram = 512
   cpu = 1
   antlet_num = 30 # IP
   zpool_name = "antlets"
@@ -39,7 +39,7 @@ resource "antsle_antlets" "master-1" {
 resource "antsle_antlets" "master-2" {
   dname = "k8s1-master-2"
   template = "CentOS-7"
-  ram = 1024
+  ram = 512
   cpu = 1
   antlet_num = 12 # IP
   zpool_name = "antlets"
@@ -50,7 +50,7 @@ resource "antsle_antlets" "master-2" {
 resource "antsle_antlets" "worker-0" {
   dname = "k8s1-worker-0"
   template = "CentOS-7"
-  ram = 2024
+  ram = 4000
   cpu = 2
   antlet_num = 20 # IP
   zpool_name = "antlets"
@@ -61,7 +61,7 @@ resource "antsle_antlets" "worker-0" {
 resource "antsle_antlets" "worker-1" {
   dname = "k8s1-worker-1"
   template = "CentOS-7"
-  ram = 2024
+  ram = 4000
   cpu = 2
   antlet_num = 21 # IP
   zpool_name = "antlets"
@@ -72,7 +72,7 @@ resource "antsle_antlets" "worker-1" {
 resource "antsle_antlets" "worker-2" {
   dname = "k8s1-worker-2"
   template = "CentOS-7"
-  ram = 2024
+  ram = 4000
   cpu = 2
   antlet_num = 22 # IP
   zpool_name = "antlets"
